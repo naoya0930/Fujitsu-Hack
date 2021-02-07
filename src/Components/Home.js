36925f73e497a23b760e47db2e68335b199ce074
naoya0930 <utmost.gt.y.rgx@gmail.com>
2021-02-07 10:20:32 +0900

    homeのデザイン修正

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -66,7 +66,9 @@ const Home=(props)=>{
                     <Button variant="contained" color="primary" onClick={loginCheck}>
                         教員用サービスはこちら
                     </Button>
+                    
                 </Grid>
+                <h1>___</h1>
             </Grid>
         </div>
         </div>
